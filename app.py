73eeda9587b1a093eabbdea32b6e070bb5fd0821
theoWlkr <theo.walcker@gmail.com>
2020-09-14 16:36:59 +0200

    Create branch + first class

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from voiceProcessing import VoiceProcessing
 
 def main():
 	print('Hello world !')
+	voice_process = VoiceProcessing()
+	voice_process.from_audio()
 
 if __name__ == "__main__":
 	main()
