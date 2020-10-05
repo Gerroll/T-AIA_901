33607cf9c7609f87ea7a3a30075e571821aea7dd
mdufour <mathieu.dufour@whereyoulove.com>
2020-10-05 17:26:38 +0200

    fix json request error and return json motherfucker

--- app.py
+++ app.py
@@ -1,13 +1,62 @@
 from naturalLanguageProcessing import Nlp
 from pathFindingProcessing import PathFinder
 from voiceProcessing import VoiceProcessing
+
+# Flask
 from flask import Flask
+from flask import request
+from flask import abort
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+""" Just home route """
+@app.route('/', methods=['GET'])
+def hello():
+  return 'Hello World, do you like trains ?'
 
+""" Webhook for facebook chatbot """
+@app.route('/webhook', methods=['GET', 'POST'])
+def main_entry():
+  # Verify token webhook to discuss with the chatbot
+  if request.method == 'GET': 
+    # Your verify token. Should be a random string.
+    VERIFY_TOKEN = "EAAwCqdXoSnYBANZCKxZAZAp7Xu4bwwJ6ZCUSIEa7gHvQt55oDielASlZB6ipIwdZCsNTkysCK3eYZCZCZAnowwRXz2twu4RTo1yWvfm77cGsGu6XMlAgXtN89Nerqg5iQhUiLq54DxP4j57xOY3BqNlzchEzeP740wAP8IhROfhLyXwZDZD"
+    # Parse the query params
+    mode = request.args.get('hub.mode')
+    token = request.args.get('hub.verify_token')
+    challenge = request.args.get('hub.challenge')
+
+    # Checks if a token and mode is in the query string of the request
+    if mode and token:
+      # Checks the mode and token sent is correct
+      if mode == 'subscribe' and token == VERIFY_TOKEN:
+        # Responds with the challenge token from the request
+        print('WEBHOOK_VERIFIED')
+        return challenge
+      else:
+        # Responds with '403 Forbidden' if verify tokens do not match
+        abort(403)
+
+  # We can talk to the chatbot
+  elif request.method == 'POST':
+    data = request.get_json(force=True)
+    print(data)
+    # Checks this is an event from a page subscription
+    if data['object'] == 'page':
+      # Iterates over each entry - there may be multiple if batched
+      for entry in data['entry']:
+        # Gets the message. entry.messaging is an array, but will only ever contain one message, so we get index 0
+        webhook_event = entry['messaging'][0]
+        print(webhook_event)
+
+      # Returns a '200 OK' response to all requests
+      return 'EVENT_RECEIVED'
+    else:
+      # Returns a '404 Not Found' if event is not from a page subscription
+      abort(404)
+
+
+
+""" Main program """
 def main():
 	print('Hello world !')
 	NLP = Nlp()
