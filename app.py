8896e88fc579bd6a863bc7d969d5f076e572f603
theoWlkr <theo.walcker@gmail.com>
2020-10-12 16:13:57 +0200

    Requirement

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ import speech_recognition as sr
 def main():
 	voice_process = VoiceProcessing()
 	try:
+		#Usecase: handling from a microphone
 		#resultFromVoice = voice_process.from_audio()
+
 		#Usecase: handling from an audiofile
 	    resultFromVoice = voice_process.from_file(pathfile="oss117.mp4")
 
@@ -23,42 +25,42 @@ def main():
 
 	print('Hello world !')
 
-	NLP = Nlp()
-	NLP.train()
-
-	# example a virer lors de l association des components
-	try:
-		print(NLP.predict("Je souhaiterai aller à Besancon"))
-	except Exception as identifier:
-		print("Bad Phrase")
-	try:
-		print(NLP.predict("je veux un itinéraire pour faire Montpellier - Nice"))
-	except Exception as identifier:
-		print("Bad Phrase")
-	try:
-		print(NLP.predict("je veux un itinéraire pour faire Paris - Brest"))
-	except Exception as identifier:
-		print("Bad Phrase")
-	try:
-		print(NLP.predict("je veux manger un Paris - Brest"))
-	except Exception as identifier:
-		print("Bad Phrase")
-	try:
-		print(NLP.predict("je veux manger une saucisse de Strasbourg à Brest"))
-	except Exception as identifier:
-		print("Bad Phrase")
-	try:
-		print(NLP.predict("quel est le meilleur trajet pour aller de Montpellier à Nice"))
-	except Exception as identifier:
-		print("Bad Phrase")
-	try:
-		print(NLP.predict("quel est le meilleur trajet pour aller de Nice à Montpellier"))
-	except Exception as identifier:
-		print("Bad Phrase")
-	try:
-		print(NLP.predict("Paris est la meilleure ville"))
-	except Exception as identifier:
-		print("Bad Phrase")
+	# NLP = Nlp()
+	# NLP.train()
+	#
+	# # example a virer lors de l association des components
+	# try:
+	# 	print(NLP.predict("Je souhaiterai aller à Besancon"))
+	# except Exception as identifier:
+	# 	print("Bad Phrase")
+	# try:
+	# 	print(NLP.predict("je veux un itinéraire pour faire Montpellier - Nice"))
+	# except Exception as identifier:
+	# 	print("Bad Phrase")
+	# try:
+	# 	print(NLP.predict("je veux un itinéraire pour faire Paris - Brest"))
+	# except Exception as identifier:
+	# 	print("Bad Phrase")
+	# try:
+	# 	print(NLP.predict("je veux manger un Paris - Brest"))
+	# except Exception as identifier:
+	# 	print("Bad Phrase")
+	# try:
+	# 	print(NLP.predict("je veux manger une saucisse de Strasbourg à Brest"))
+	# except Exception as identifier:
+	# 	print("Bad Phrase")
+	# try:
+	# 	print(NLP.predict("quel est le meilleur trajet pour aller de Montpellier à Nice"))
+	# except Exception as identifier:
+	# 	print("Bad Phrase")
+	# try:
+	# 	print(NLP.predict("quel est le meilleur trajet pour aller de Nice à Montpellier"))
+	# except Exception as identifier:
+	# 	print("Bad Phrase")
+	# try:
+	# 	print(NLP.predict("Paris est la meilleure ville"))
+	# except Exception as identifier:
+	# 	print("Bad Phrase")
 
 
 if __name__ == "__main__":
