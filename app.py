4f2618b2e28c4b2efc4dde1adcd73017ace1e109
mdufour <mathieu.dufour@whereyoulove.com>
2020-10-12 17:42:49 +0200

    try generic template as response

--- app.py
+++ app.py
@@ -87,53 +87,29 @@ def main_entry():
                     "payload": {
                       "template_type": "list",
                       "top_element_style": "compact",
-                      "elements": [
+                      "elements":[
                         {
-                          "title": "Classic T-Shirt Collection",
-                          "subtitle": "See all our colors",
-                          "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
-                          "buttons": [
-                            {
-                              "title": "View",
-                              "type": "web_url",
-                              "url": "https://peterssendreceiveapp.ngrok.io/collection",
-                              "messenger_extensions": True,
-                              "webview_height_ratio": "tall",
-                              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-                            }
-                          ]
-                        },
-                        {
-                          "title": "Classic White T-Shirt",
-                          "subtitle": "See all our colors",
+                          "title":"Welcome!",
+                          "image_url":"",
+                          "subtitle":"We have the right hat for everyone.",
                           "default_action": {
                             "type": "web_url",
-                            "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                            "url": "",
                             "messenger_extensions": False,
-                            "webview_height_ratio": "tall"
-                          }
-                        },
-                        {
-                          "title": "Classic Blue T-Shirt",
-                          "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                          "subtitle": "100% Cotton, 200% Comfortable",
-                          "default_action": {
-                            "type": "web_url",
-                            "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                            "messenger_extensions": True,
                             "webview_height_ratio": "tall",
-                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                            "fallback_url": ""
                           },
-                          "buttons": [
+                          "buttons":[
                             {
-                              "title": "Shop Now",
-                              "type": "web_url",
-                              "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                              "messenger_extensions": True,
-                              "webview_height_ratio": "tall",
-                              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-                            }
-                          ]        
+                              "type":"web_url",
+                              "url":"",
+                              "title":"View Website"
+                            },{
+                              "type":"postback",
+                              "title":"Start Chatting",
+                              "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                            }              
+                          ]      
                         }
                       ],
                       "buttons": [
