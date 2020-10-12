b8fd675875a960c893984f10d6e18554d01c4649
mdufour <mathieu.dufour@whereyoulove.com>
2020-10-13 01:57:18 +0200

    add get started + change access token and recipient id

--- app.py
+++ app.py
@@ -56,6 +56,20 @@ def main_entry():
         print(webhook_data)
         print(recipient_id)
 
+        # Get started
+        if 'postback' in webhook_data and webhook_data['postback']['payload'] == 'GET_STARTED':
+          # Send a text messagee explaining the chatbot
+          payload = {
+            "messaging_type": "RESPONSE",
+            "recipient": {
+              "id": recipient_id
+            },
+            "message": {
+              "text": "Bienvenue sur ce magnifique chatbot !\nIl permet de trouver les trains les plus rapides entre 2 villes.\nAttention, il nee comprend que les messages audios.\nPour commencer à l'utiliser, envoyez un message, comme par exemple : 'Je veux aller de Paris jusqu'à Montpellier.'"
+          }
+          response = requests.post('https://graph.facebook.com/v2.6/me/messages?access_token={0}'.format(ACCESS_TOKEN), data=payload)
+          print(response.json())
+
         if 'message' in webhook_data:
           if 'attachments' in webhook_data['message']:
             attachment = webhook_data['message']['attachments'][0]
@@ -98,7 +112,7 @@ def main_entry():
               }
 
               # Send the result as a list template message 
-              response = requests.post('https://graph.facebook.com/v2.6/me/messages?access_token={0}'.format(VERIFY_TOKEN), json=payload)
+              response = requests.post('https://graph.facebook.com/v2.6/me/messages?access_token={0}'.format(ACCESS_TOKEN), data=payload)
 
               print(response.json())
 
